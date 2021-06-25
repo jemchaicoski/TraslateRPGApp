99358c1c23cf604b96fbc2e2ff1fda716ff12c1a
jemchaicoski <57416346+jemchaicoski@users.noreply.github.com>
2021-06-24 23:55:07 -0300

    Buttons and Image of main screen created

--- translate_rpg_app/lib/main.dart
+++ translate_rpg_app/lib/main.dart
@@ -37,18 +37,76 @@ class _MyHomePageState extends State<MyHomePage> {
 
       body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new CircleAvatar(
-              radius: 100.0,
-              backgroundImage: AssetImage("images/icons/dragon-remade.png"),
-              backgroundColor: Colors.amber[500],
-            ),
+          children: [
+            ImageIconWidget(),
+            CharacterInformationWidget(),
           ],
         ),
       ),
+      backgroundColor: Colors.amber[200],
       //bottomNavigationBar: NavegationBar(),
+    );
+  }
+}
+
+class ImageIconWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 100.0,
+      backgroundImage: AssetImage("images/icons/knight.png"),
+      backgroundColor: Colors.yellow[300],
+    );
+  }
+}
+
+class CharacterInformationWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 5.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //mainAxisSize: MainAxisSize,
+        children: [
+          ButtonsCharacterSheetWidget(
+            labelText: "Ficha",
+            imagePath: "images/icons/status.png",
+          ),
+          ButtonsCharacterSheetWidget(
+            labelText: "Habilidades",
+            imagePath: "images/icons/book.png",
+          ),
+          ButtonsCharacterSheetWidget(
+            labelText: "Inventário",
+            imagePath: "images/icons/backpack.png",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonsCharacterSheetWidget extends StatelessWidget {
+  final String labelText;
+  final String imagePath;
+
+  ButtonsCharacterSheetWidget(
+      {@required this.labelText, @required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => {},
+      child: Column(
+        children: <Widget>[
+          Image(
+            image: AssetImage(imagePath),
+            width: 50.0,
+          ),
+          Text(labelText),
+        ],
+      ),
     );
   }
 }
