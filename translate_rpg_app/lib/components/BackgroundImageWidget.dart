556fb61364ed144acc56ef262853229cc6fd743d
jemchaicoski <57416346+jemchaicoski@users.noreply.github.com>
2021-11-02 19:40:10 -0300

    Gradient dark background

--- translate_rpg_app/lib/components/BackgroundImageWidget.dart
+++ translate_rpg_app/lib/components/BackgroundImageWidget.dart
@@ -11,15 +11,29 @@ class BackgroundImageWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: image,
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.2), BlendMode.darken),
+  Widget build(BuildContext context) => Stack(
+        children: [
+          buildBackground(),
+          child,
+        ],
+      );
+
+  Widget buildBackground() => ShaderMask(
+        shaderCallback: (bounds) => LinearGradient(
+          colors: [Colors.black26, Colors.black87],
+          begin: Alignment.center,
+          end: Alignment.bottomCenter,
+        ).createShader(bounds),
+        blendMode: BlendMode.darken,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: image,
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.2), BlendMode.darken),
+            ),
           ),
         ),
-        child: child,
       );
 }
